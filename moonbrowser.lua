784925a04bcd0c635502273a68cf65ac43a60f87
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-02-08 03:26:38 -0400

    ...

--- moonbrowser.lua
+++ moonbrowser.lua
@@ -7,10 +7,10 @@
  @date      06.02.2021 08:50:42 -04
 --]]
 
-lgi		  = require("lgi")
-Gtk		  = lgi.require('Gtk', '3.0')
-Gdk		  = lgi.require('Gdk', '3.0')
-GLib	  = lgi.require('GLib', '2.0')
+lgi       = require("lgi")
+Gtk       = lgi.require('Gtk', '3.0')
+Gdk       = lgi.require('Gdk', '3.0')
+GLib      = lgi.require('GLib', '2.0')
 Webkit    = lgi.require('WebKit2','4.0')
 
 app       = Gtk.Application()
